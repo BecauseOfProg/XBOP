6b2b08374f6413141e14dbc7688523550fda56ac
Théo Vidal <exybore@becauseofprog.fr>
2021-01-06 19:10:06 +0100

    feat: add temporary hack to support slash commands
    Added support for new slash commands in a very dirty manner. Waiting for bwmarrin/discordgo#856 to be merged so they'll be supported in the official library.

---
+++ games/irregular_verbs/interaction.go
@@ -0,0 +1,15 @@
+package irregular_verbs
+
+import (
+	"context"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/theovidal/onyxcord"
+)
+
+func HandleInteraction(bot *onyxcord.Bot, message *discordgo.Message) {
+	verbsPlayer := bot.Cache.Exists(context.Background(), "verbs:"+message.ChannelID).Val()
+	if verbsPlayer == 1 {
+		HandleAnswer(bot, message, "verbs:"+message.ChannelID)
+	}
+}
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/bwmarrin/discordgo"
 	"github.com/theovidal/onyxcord"
 
@@ -13,11 +14,26 @@ func main() {
 	bot.RegisterCommand("verbs", irregular_verbs.Command())
 	irregular_verbs.VerbsPlayers.Initialize()
 
-	bot.Client.AddHandler(func(session *discordgo.Session, message *discordgo.MessageCreate) {
-		verbsPlayer, exists := irregular_verbs.VerbsPlayers.GetPlayer(message.ChannelID)
-		if exists {
-			irregular_verbs.HandleAnswer(&bot, message.Message, verbsPlayer)
+	bot.Client.AddHandler(func(session *discordgo.Session, event *discordgo.Event) {
+		if event.Type == "INTERACTION_CREATE" {
+			data := make(map[string]interface{})
+			json.Unmarshal(event.RawData, &data)
+			command := data["data"].(map[string]interface{})["name"].(string)
+			if command == "verbs" {
+				channelID := data["channel_id"].(string)
+				event := discordgo.MessageCreate{
+					Message: &discordgo.Message{
+						ChannelID: channelID,
+						Author:    &discordgo.User{},
+					},
+				}
+				irregular_verbs.Command().Execute([]string{}, bot, &event)
+			}
 		}
+	})
+
+	bot.Client.AddHandler(func(session *discordgo.Session, message *discordgo.MessageCreate) {
+		irregular_verbs.HandleInteraction(&bot, message.Message)
 		bot.OnCommand(session, message)
 	})
 
